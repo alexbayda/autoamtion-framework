a7b61dd2897dc89aa736be0a01c2b80b93d458c9
alexbayda <alex.bayda@yahoo.ca>
2023-02-06 23:03:44 +0200

    stabilization

--- src/test/java/com/herokuapp/katalon/StreamTest.java
+++ src/test/java/com/herokuapp/katalon/StreamTest.java
@@ -1,15 +1,22 @@
-//package com.herokuapp.katalon;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class StreamTest {
-//
+package com.herokuapp.katalon;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StreamTest {
+
+    public static void main(String[] args){
+        String s1 = "00";
+        String s2 = new String();
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+    }
+
 //    static List<Integer> readFile() throws FileNotFoundException {
 //        Scanner scanner = new Scanner(new File("src/main/resources/text.txt"));
 //        List<Integer> tall = new ArrayList<Integer>();
@@ -32,5 +39,6 @@
 //        System.out.println(computedAges);
 //
 //    }
-//
-//}
+
+
+}
