c150d18e08b53d1d01ddb2b40a3ff28f0685dcab
alexbayda <alex.bayda@yahoo.ca>
2023-04-27 14:09:48 -0400

    add grid

--- src/main/java/com/herokuapp/katalon/driver/Grid.java
+++ src/main/java/com/herokuapp/katalon/driver/Grid.java
@@ -1,7 +1,7 @@
 package com.herokuapp.katalon.driver;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -14,7 +14,7 @@ public class Grid {
 
         @Test
         void setUp() throws MalformedURLException {
-                FirefoxOptions options = new FirefoxOptions();
+                EdgeOptions options = new EdgeOptions();
                 driver = new RemoteWebDriver(new URL("http://192.168.0.193:4444"), options);
                 driver.get("https://google.com");
         }
